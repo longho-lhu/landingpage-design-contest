c3bfcbf397f1a16de1cbaa98c8338e7168b5b308
NamHoang04 <nam.vuonghoang04@gmail.com>
2024-11-09 13:30:39 +0700

    add component Header

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './Component/Header';
 
 test('renders learn react link', () => {
   render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<Header />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
